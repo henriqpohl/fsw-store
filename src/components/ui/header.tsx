ffd3d57bc0b401fe3665712a644d8f08aba25030
Henrique R. Pohl <henriqpohl@gmail.com>
2023-10-18 18:14:05 +0100

    feat: ensure user goes to home page on logo click.

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -14,7 +14,13 @@ import {
 // Shadcn Components
 import { Button } from "./button";
 import { Card } from "./card";
-import { Sheet, SheetClose, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTrigger,
+} from "./sheet";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { Avatar, AvatarImage } from "./avatar";
 import { AvatarFallback } from "@radix-ui/react-avatar";
@@ -115,9 +121,11 @@ const Header = () => {
         </SheetContent>
       </Sheet>
 
-      <h1 className="text-lg font-semibold">
-        <span className="text-primary">FWS</span> Store
-      </h1>
+      <Link href="/">
+        <h1 className="text-lg font-semibold">
+          <span className="text-primary">FWS</span> Store
+        </h1>
+      </Link>
 
       <Button size="icon" variant="outline">
         <ShoppingCartIcon />
