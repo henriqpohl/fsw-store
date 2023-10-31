a967560d95c4cd58614b43e4a0ca96136dd67faf
Henrique R. Pohl <henriqpohl@gmail.com>
2023-10-31 10:18:13 +0000

    feat: add DiscountBadge component.

--- src/app/product/[slug]/components/product-info.tsx
+++ src/app/product/[slug]/components/product-info.tsx
@@ -2,8 +2,14 @@
 
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import DiscountBadge from "@/components/ui/discount-badge";
 import { ProductWithTotalPrice } from "@/helpers/product";
-import { ArrowDownIcon, ArrowLeftIcon, ArrowRightIcon, TruckIcon } from "lucide-react";
+import {
+  ArrowDownIcon,
+  ArrowLeftIcon,
+  ArrowRightIcon,
+  TruckIcon,
+} from "lucide-react";
 import { useState } from "react";
 
 interface ProductInfoProps {
@@ -32,9 +38,7 @@ const ProductInfo = ({
       <div className="flex items-center gap-1">
         <h1 className="text-lg font-bold">R$ {totalPrice.toFixed(2)}</h1>
         {discountPercentage > 0 && (
-          <Badge className="px-2 py-[2px]">
-            <ArrowDownIcon size={14} /> {discountPercentage}%
-          </Badge>
+          <DiscountBadge>{discountPercentage}</DiscountBadge>
         )}
       </div>
 
@@ -73,7 +77,7 @@ const ProductInfo = ({
         Adicionar ao carrinho
       </Button>
 
-      <div className="mt-5 flex items-center justify-between bg-accent rounded-lg px-5 py-2">
+      <div className="mt-5 flex items-center justify-between rounded-lg bg-accent px-5 py-2">
         <div className="flex items-center">
           <TruckIcon />
           <div className="flex flex-col">
