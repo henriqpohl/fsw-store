11de9b0edebfc74b9b06f9cbb4188f9d437c9ae8
Henrique R. Pohl <henriqpohl@gmail.com>
2023-10-31 12:10:55 +0000

    feat: ensure product quantity can be increased from cart.

--- src/providers/cart.tsx
+++ src/providers/cart.tsx
@@ -15,6 +15,7 @@ interface ICartContext {
   cartTotalDiscount: number;
   addProductsToCart: (product: CartProduct) => void;
   decreaseProductQuantity: (productId: string) => void;
+  increaseProductQuantity: (productId: string) => void;
 }
 
 export const CartContext = createContext<ICartContext>({
@@ -24,6 +25,7 @@ export const CartContext = createContext<ICartContext>({
   cartTotalDiscount: 0,
   addProductsToCart: () => {},
   decreaseProductQuantity: () => {},
+  increaseProductQuantity: () => {},
 });
 
 const CartProvider = ({ children }: { children: ReactNode }) => {
@@ -77,12 +79,32 @@ const CartProvider = ({ children }: { children: ReactNode }) => {
     );
   };
 
+  const increaseProductQuantity = (productId: string) => {
+    // se a quantidade for 1, remova o produto do carrinho
+
+    // se não, diminua a quantidade em 1
+
+    setProducts((prev) =>
+      prev
+        .map((cartProduct) => {
+          if (cartProduct.id === productId) {
+            return {
+              ...cartProduct,
+              quantity: cartProduct.quantity + 1,
+            };
+          }
+          return cartProduct;
+        })
+    );
+  };
+
   return (
     <CartContext.Provider
       value={{
         products,
         addProductsToCart,
         decreaseProductQuantity,
+        increaseProductQuantity,
         cartTotalPrice: 0,
         cartBasePrice: 0,
         cartTotalDiscount: 0,
