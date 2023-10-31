3806c2bfb1b0672c8da2f344509262e43fbeacef
Henrique R. Pohl <henriqpohl@gmail.com>
2023-10-31 09:31:30 +0000

    feat: ensure recommended products are shown on product details page.

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Categories from "./components/categories";
 import { prismaClient } from "@/lib/prisma";
-import ProductList from "./components/product-list";
+import ProductList from "../../components/ui/product-list";
 import SectionTitle from "./components/section-title";
 import PromoBanner from "./components/promo-banner";
 
@@ -67,7 +67,6 @@ export default async function Home() {
         <SectionTitle>Teclados</SectionTitle>
         <ProductList products={mouses} />
       </div>
-
     </div>
   );
 }
