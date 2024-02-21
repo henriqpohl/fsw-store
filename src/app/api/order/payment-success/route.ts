2d97cd6ec67544da2c8a59b88c4125aad78868b3
henriqpohl <henriqpohl@gmail.com>
2024-02-21 15:19:27 +0000

    feat: add stripe webhook

---
+++ src/app/api/order/payment-success/route.ts
@@ -0,0 +1,42 @@
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "2023-10-16",
+});
+
+export const POST = async (request: Request) => {
+  // Verificar se este evente é nosso, por questao de segurança
+  const signature = request.headers.get("stripe-signature");
+
+  if (!signature) {
+    return NextResponse.error();
+  }
+
+  const text = await request.text();
+
+  const event = stripe.webhooks.constructEvent(
+    text,
+    signature,
+    process.env.STRIPE_WEBHOOK_SECRET_KEY,
+  );
+
+  // Escutando o evento de sucesso do Stripe
+  if (event.type === "checkout.session.completed") {
+    const session = event.data.object as Stripe.Checkout.Session;
+
+    const sessionWithLineItems = await stripe.checkout.sessions.retrieve(
+      event.data.object.id,
+      {
+        expand: ["line_items"],
+      },
+    );
+    const lineItems = sessionWithLineItems.line_items;
+
+    console.log(lineItems);
+
+    // CRIAR PEDIDO
+  }
+
+  return NextResponse.json({ received: true });
+};
