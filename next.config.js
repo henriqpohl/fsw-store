8b07041fd9dc5edd3a667d3c15646786f9c5f3e8
henriqpohl <henriqpohl@gmail.com>
2024-02-21 20:33:33 +0000

    fix: add image remote patterns hostname

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["fsw-store.s3.sa-east-1.amazonaws.com"],
+    remotePatterns: [
+      {
+        hostname: "utfs.io",
+      },
+    ],
   },
   experimental: {
     serverActions: true,
